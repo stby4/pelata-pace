6aea432bcf1eeb3aa02a9cf756fec93ca3caebc2
Hinrich Kaestner <hinrich.kaestner@outlook.com>
2022-10-12 23:37:28 +0200

    Initial changes to pace formulas

--- src/jvmMain/kotlin/net/pelata/features/pace/model/Split.kt
+++ src/jvmMain/kotlin/net/pelata/features/pace/model/Split.kt
@@ -15,8 +15,10 @@ class Split(
         val distance: Double,
         val time: Double,
         val unit: Distance = Distance.KILOMETERS,
+        val splitLength: Int = 1,
         val precision: Int = 100
 ) {
+    private val precisionCorrected = precision * splitLength
     val averagePace: Double by lazy { time / distance }
     val averageSpeed: Double by lazy { distance / (time / 60) }
     val averageSpeedKmh: Double by lazy {
@@ -29,16 +31,16 @@ class Split(
         assert(percentage >= 0.0 && percentage <= 1.0)
 
         val slowest = averagePace + averagePace * percentage
-        val segments = ceil(distance).toInt()
-        val distances = DoubleArray(segments) { 1.0 }
+        val segments = ceil(distance / splitLength).toInt()
+        val distances = DoubleArray(segments) { splitLength.toDouble() }
         if (distance - floor(distance) > 0) {
             distances[distances.size - 1] = distance - floor(distance)
         }
 
-        val step = -2 * averagePace * percentage / (distance * precision)
+        val step = -2 * averagePace * percentage / (distance * precisionCorrected)
         val times =
                 distances.mapIndexed { idx, distance ->
-                    SplitTime(calcSplitPace(idx, slowest, precision, distance, step))
+                    SplitTime(calcSplitPace(idx, slowest, distance, step))
                 }
 
         return SplitTimeList(times)
@@ -61,22 +63,17 @@ class Split(
      * of 1, and the last one has the remaining distance to the full track distance.
      */
     fun distances(): SplitDistanceList {
-        val distances = DoubleArray(ceil(distance).toInt()) { 1.0 }
+        val distances = DoubleArray(ceil(distance / splitLength).toInt()) { splitLength.toDouble() }
         if (distance - floor(distance) > 0) {
             distances[distances.size - 1] = distance - floor(distance)
         }
 
-        val distancesSum = distances.mapIndexed { idx, distance -> 1.0 * idx + distance }
+        val distancesSum = distances.mapIndexed { idx, distance -> splitLength * idx + distance }
 
         return SplitDistanceList(distancesSum)
     }
 
     /** Calculates the pace for a given split (defined by idx). */
-    internal fun calcSplitPace(
-            idx: Int,
-            slowest: Double,
-            precision: Int,
-            distance: Double,
-            step: Double
-    ): Double = slowest + (precision * step * (idx + distance / 2))
+    internal fun calcSplitPace(idx: Int, slowest: Double, distance: Double, step: Double): Double =
+            slowest + (precisionCorrected * step * splitLength.toDouble() * (idx + distance / 2))
 }
--- src/jvmTest/kotlin/net/pelata/SplitTest.kt
+++ src/jvmTest/kotlin/net/pelata/SplitTest.kt
@@ -58,27 +58,54 @@ class SplitTest {
     @Test
     fun testCalcSplitPace() {
         val split = Split(10.0, 60.0)
-        
-        val split0 = split.calcSplitPace(0, 6.0, 100, 1.0, 0.02)
+
+        val split0 = split.calcSplitPace(0, 6.0, 1.0, 0.02)
         assertEquals(7.0, split0, 0.0001)
 
-        val split1 = split.calcSplitPace(1, 6.0, 100, 1.0, 0.02)
+        val split1 = split.calcSplitPace(1, 6.0, 1.0, 0.02)
         assertEquals(9.0, split1, 0.0001)
 
-        val split2 = split.calcSplitPace(2, 6.0, 100, 0.33, 0.02)
+        val split2 = split.calcSplitPace(2, 6.0, 0.33, 0.02)
         assertEquals(10.33, split2, 0.0001)
     }
 
     @Test
     fun testNegativeSplits() {
-        val split = Split(10.33, 60.0, Distance.KILOMETERS, 10)
+        val split = Split(10.33, 60.0, Distance.KILOMETERS, 1, 10)
 
         val splits = split.negativeSplits(0.05).splitTimes
 
         assertEquals(6.07, splits[0].duration, 0.01)
 
-        assertEquals(5.79, splits[5].duration, 0.01)        
+        assertEquals(5.79, splits[5].duration, 0.01)
 
         assertEquals(5.518, splits[10].duration, 0.01)
+
+        assertEquals(60.0, splits.sumOf { it.duration } - 0.67 * splits[10].duration, 0.1)
+    }
+
+    @Test
+    fun testVariableSplitLength() {
+        val split = Split(10.33, 60.0, Distance.KILOMETERS, 5)
+
+        val splitDistances = split.distances().splitDistances
+        assertEquals(3, splitDistances.size)
+        assertEquals(5.0, splitDistances[0], 0.0001)
+        assertEquals(10.0, splitDistances[1], 0.0001)
+        assertEquals(10.33, splitDistances[2], 0.0001)
+
+        val splits = split.negativeSplits(0.05).splitTimes
+
+        assertEquals(5.39, splits[0].duration, 0.01)
+
+        assertEquals(5.11, splits[1].duration, 0.01)
+
+        assertEquals(5.49, splits[2].duration, 0.01)
+
+        assertEquals(
+                60.0,
+                5.0 * splits[0].duration + 5.0 * splits[1].duration + 0.33 * splits[2].duration,
+                0.1
+        )
     }
 }
