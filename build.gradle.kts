0dddde1a7aa96925965b3c6143e91c9912e576d2
Hinrich Kaestner <hinrich.kaestner@outlook.com>
2023-08-03 12:14:42 +0200

    Add linter to project

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
     application
     id("io.ktor.plugin") version "2.3.2"
     id("io.gitlab.arturbosch.detekt") version "1.23.0"
+    id("org.jlleitschuh.gradle.ktlint") version "11.5.0"
 }
 
 group = "net.pelata"
